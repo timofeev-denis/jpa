9b202727e86eca1252c6c00ecb23ae4a52532841
Denis Timofeev <nakolenke@gmail.com>
2018-06-02 16:08:41 +0300

    Add swagger

--- src/main/java/ru/code4fun/demo/jpa/EmployeeController.java
+++ src/main/java/ru/code4fun/demo/jpa/EmployeeController.java
@@ -3,9 +3,11 @@ package ru.code4fun.demo.jpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
---
+++ src/main/java/ru/code4fun/demo/jpa/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by: Denis Timofeev
+ * Date: 02.06.2018
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
